7dd78715ea7ffc7323bfdb641a5f486d9b9a4737
senyapapa <takeblec@gmail.com>
2026-01-14 10:53:42 +0300

    feat(generator): Implement basic file copying from embedded templates - Add simple templates - Add render function. Implement recursiver copying of static files   using fs.WalkDir + fs.Sub.

--- internal/generator/render.go
+++ internal/generator/render.go
@@ -2,6 +2,8 @@ package generator
 
 import (
 	"embed"
+	"fmt"
+	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -9,45 +11,89 @@ import (
 	"text/template"
 )
 
-//NOTE: I tested this code and it works fine but i think it needs refactoring later
+type Options struct {
+	DryRun         bool
+	ForceOverWrite bool
+	GoModuleName   string
+}
 
-//go:embed template/**/*
-var templateEmbed embed.FS
-var templateFs fs.FS = templateEmbed
+type ProjectGenerator struct {
+	opts       Options
+	templateFs fs.FS
+	infoLog    io.Writer
+	errorLog   io.Writer
+}
 
-func RenderProject(targetDir string, templateFs fs.FS, data any) error {
-	return fs.WalkDir(templateFs, ".", func(path string, d fs.DirEntry, err error) error {
+//go:embed template/*
+var defaultFS embed.FS
+
+func NewProjectGenerator(opts Options, userTemplateFs fs.FS) *ProjectGenerator {
+	var templateFs fs.FS
+	//If the user has not specified a template we use the system template.
+	if userTemplateFs == nil {
+		templateFs = defaultFS
+	} else {
+		templateFs = userTemplateFs
+	}
+	return &ProjectGenerator{
+		opts:       opts,
+		templateFs: templateFs,
+		infoLog:    os.Stdout,
+		errorLog:   os.Stderr,
+	}
+}
+
+func (g *ProjectGenerator) Render(targetDir string) error {
+	rootFs, err := fs.Sub(g.templateFs, "template")
+	if err != nil {
+		return fmt.Errorf("Failed to create sub FS: %w", err)
+	}
+
+	return fs.WalkDir(rootFs, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
+
 		targetPath := filepath.Join(targetDir, path)
 
+		// Creating dir
 		if d.IsDir() {
 			return os.MkdirAll(targetPath, 0755)
 		}
 
-		content, err := fs.ReadFile(templateFs, path)
+		// Copying file
+		sourseContent, err := fs.ReadFile(rootFs, path)
 		if err != nil {
 			return err
 		}
 
-		if strings.HasSuffix(path, ".tmp") {
-			targetPath = strings.TrimSuffix(targetPath, ".tmp")
-
-			tmpl, err := template.New(path).Parse(string(content))
-			if err != nil {
-				return err
-			}
-
-			f, err := os.Create(targetPath)
-			if err != nil {
-				return err
-			}
-			defer f.Close()
-
-			return tmpl.Execute(f, data)
+		if strings.HasSuffix(path, ".tmpl") {
+			targetPath = strings.TrimSuffix(targetPath, ".tmpl")
+			return g.proccessTemplate(targetPath, string(sourseContent), g.opts)
 		}
 
-		return os.WriteFile(targetPath, content, 0644)
+		return os.WriteFile(targetPath, sourseContent, 0644)
+
 	})
+
+}
+
+func (g *ProjectGenerator) proccessTemplate(targetPath string, sourceConetent string, data any) error {
+	tmpl, err := template.New(filepath.Base(targetPath)).Parse(sourceConetent)
+	if err != nil {
+		return fmt.Errorf("Failed to parse template %s: %w", targetPath, err)
+	}
+
+	f, err := os.Create(targetPath)
+	if err != nil {
+		return fmt.Errorf("Failed to create file %s: %w", targetPath, err)
+	}
+	defer f.Close()
+
+	if err := tmpl.Execute(f, data); err != nil {
+		_ = os.Remove(targetPath)
+		return fmt.Errorf("Failed to execute template %s: %w", targetPath, err)
+	}
+
+	return nil
 }
